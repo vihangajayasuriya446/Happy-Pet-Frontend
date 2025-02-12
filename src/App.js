c08e5dbe913d54897a24ba1c225e832c7d43a836
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-02-12 22:58:48 +0530

    App.js added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the HappyPet</h1>
+        <button className='user-button' onClick={() => navigate('/users')}>
+          Users
+        </button>
+
+        <button className='user-button2' onClick={() => navigate('/mainpage')}>
+          Main Page
+        </button>
       </header>
     </div>
   );
