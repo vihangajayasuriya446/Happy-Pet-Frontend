4e81b11a38d322d7565e5e074f18d2744830f233
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-02-12 22:59:54 +0530

    Mainuser added

---
+++ src/MainUser.js
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import MainPage from './MainPage';
+import Footer from './Footer';
+
+
+const MainUser = () => {
+
+    return(
+        <Box>
+            <MainPage/>
+            <Footer></Footer>
+            
+
+        </Box>
+    );
+
+
+};
+
+export default MainUser;
