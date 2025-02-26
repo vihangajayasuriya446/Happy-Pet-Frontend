6d6c2e51275b3130d4baa6581c04e316428acf89
Tharushi-Nathasha <taashiiyaa@gmail.com>
2025-02-26 20:01:23 +0530

    Added route paths to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PetGrid from './components/PetGrid';
 import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
 import { Box } from '@mui/material';
 
 const App = () => {
     return (
-        <Box sx={{ minHeight: '100vh' }}>
-            <Navbar />
-            <PetGrid />
-            <Footer />
-        </Box>
+        <Router>
+            <Box sx={{ minHeight: '100vh' }}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/adopt" element={<PetGrid />} />
+                    {/* Add more routes as needed */}
+                </Routes>
+                <Footer />
+            </Box>
+        </Router>
     );
 };
-
-
 
 export default App;
