45eb5f3e73b1bbf8a8f1f08e0200e3f208ef35ee
Tharushi-Nathasha <taashiiyaa@gmail.com>
2025-02-28 15:01:42 +0530

    Updated app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import PetGrid from './components/PetGrid';
-import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
+import Navbar from './Navbar';
+import Footer from './Footer';
 import { Box } from '@mui/material';
+import Users from './Pets';
+import HomePage from './HomePage';
+import ContactUsPage from './ContactUsPage';
+import AdoptionPage from './AdoptionPage';
 
-const App = () => {
-    return (
-        <Router>
-            <Box sx={{ minHeight: '100vh' }}>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/adopt" element={<PetGrid />} />
-                    {/* Add more routes as needed */}
-                </Routes>
-                <Footer />
-            </Box>
-        </Router>
-    );
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Box sx={{ minHeight: '100vh' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Users />} />
+          <Route path="/adopt" element={<AdoptionPage />} />
+          <Route path="/contactus" element={<ContactUsPage />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </Router>
+  );
 };
 
 export default App;
