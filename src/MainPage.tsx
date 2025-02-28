689b9b1e8dbe3e4d14671d2c88bbf7722785e496
Tharushi-Nathasha <taashiiyaa@gmail.com>
2025-02-28 15:04:42 +0530

    Added a main Page

---
+++ src/MainPage.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import MainPage from "./MainPage";
+import Footer from "./Footer";
+
+const AdoptionPage = () => {
+  return (
+    <Box>
+      <MainPage />
+      <Footer />
+    </Box>
+  );
+};
+
+export default AdoptionPage;
