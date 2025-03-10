38b7e845a956c6107a350d52a4f1e336cf768076
Hasindu0 <20222125.hasindu@iit.ac.lk>
2025-03-10 14:14:35 +0530

    New rout 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Users from './Users';
 import HomePage from './HomePage';
 import ContactUsPage from './ContactUsPage';
 import OwnerForm from './OwnerForm';
+import OwnerTable from './OwnerTable';
 
 
 const App: React.FC = () => {
@@ -21,6 +22,7 @@ const App: React.FC = () => {
           <Route path="/matchmaking" element={<MainPage />} />
           <Route path="/contactus" element={<ContactUsPage />} />
           <Route path="/OwnerForm" element={<OwnerForm />} />
+          <Route path="/owners" element={<OwnerTable />} />
         </Routes>
         <Footer />
       </Box>
