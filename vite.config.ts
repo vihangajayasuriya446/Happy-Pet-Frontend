817f772bbfd7c30c35048df921fa5a7d5f2b5ecf
Hasindu0 <hasindunishshanka@gmail.com>
2025-04-02 12:55:44 +0530

    Config_

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      filename: 'bundle-analysis.html'
-    })
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          vendor: ['axios', 'lodash'], 
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    assetsInlineLimit: 4096
-  },
-  assetsInclude: ['/.jpg', '/.png', '/.mp4', '/.webp', '/*.svg']
-});
+  plugins: [react()],
+})
